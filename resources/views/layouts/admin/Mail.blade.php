381317b3a39384e495d9f47673f66d5e3d42d1c4
iksan arifki <iksanarifki27@gmail.com>
2023-03-23 10:57:34 +0700

    create notif fix  gmail sioktig mobile

--- resources/views/layouts/admin/Mail.blade.php
+++ resources/views/layouts/admin/Mail.blade.php
@@ -1,5 +1,5 @@
 <h2>Peringatan Safety Patrol</h2>
 
-<p>Hai, hari ini ada peringatan terbaru Silahkan kamu cek Berita Peringatan <a href="/">disini</a> </p>
+<p>Hai, hari ini ada peringatan terbaru Silahkan kamu cek Berita Peringatan <a href="{{route('login') }}">disini</a> </p>
 
 <p>Jangan lupa Selesaikan Tugasnya 😉</p>
